a95a8cddc694eed8e1e2d4adf497ae06a04a5f41
directoryx <anggawijaya4567@gmail.com>
2020-03-10 19:29:19 +0700

    adding JPA Repository

---
+++ src/main/java/com/directoryxx/belajarspring/Repositories/AuthorRepository.java
@@ -0,0 +1,8 @@
+package com.directoryxx.belajarspring.Repositories;
+
+import com.directoryxx.belajarspring.Model.Author;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AuthorRepository extends CrudRepository<Author, Long> {
+
+}
---
+++ src/main/java/com/directoryxx/belajarspring/Repositories/BookRepository.java
@@ -0,0 +1,8 @@
+package com.directoryxx.belajarspring.Repositories;
+
+import com.directoryxx.belajarspring.Model.Book;
+import org.springframework.data.repository.CrudRepository;
+
+public interface BookRepository extends CrudRepository<Book, Long> {
+    
+}
